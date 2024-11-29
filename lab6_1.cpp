d549f879454a12aae071ed012bfd101770a062be
zewexqa <domtui555@gmail.com>
2024-11-29 15:15:24 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 int main(){
+    int key;
+    int even=0;
+    int odd=0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> key;
+    while(key!=0){
+    if(key%2 == 0){
+        even++;
+    }
+    else{
+        odd++;
+    }
+    cout << "Enter an integer: ";
+    cin >> key;
+    }
+    cout << "#Even numbers = "<< even << endl;
+    cout << "#Odd numbers = "<< odd << endl;
     return 0;
+
 }
